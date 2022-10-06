d24dc693b0e1c32f83cd681f1872f5c6ad7d4839
уткин никита <mr.nikita.88@mail.ru>
2022-10-06 04:58:33 +0300

    TimeFormatterTest

--- src/TimeFormatterTest.java
+++ src/TimeFormatterTest.java
@@ -8,9 +8,9 @@ public class TimeFormatterTest {
     @Test
     public void exampleTests() {
         //assertEquals("1 second", TimeFormatter.formatDuration(1));
-        //assertEquals("1 minute and 2 seconds", TimeFormatter.formatDuration(62));
-        //assertEquals("2 minutes", TimeFormatter.formatDuration(120));
-        //assertEquals("1 hour", TimeFormatter.formatDuration(3600));
+        assertEquals("1 minute and 2 seconds", TimeFormatter.formatDuration(62));
+        assertEquals("2 minutes", TimeFormatter.formatDuration(120));
+        assertEquals("1 hour", TimeFormatter.formatDuration(3600));
         assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(3662));
     }
 }
@@ -23,7 +23,37 @@ class TimeFormatter {
         long hours = TimeUnit.SECONDS.toHours(seconds) - (day *24);
         long minute = TimeUnit.SECONDS.toMinutes(seconds) - (TimeUnit.SECONDS.toHours(seconds)* 60);
         long second = TimeUnit.SECONDS.toSeconds(seconds) - (TimeUnit.SECONDS.toMinutes(seconds) *60);
+        if(day==1){
+            result.append(day + " day");
+        }else if(day>1){
+            result.append(day + " days");
+        }
+        if(day != 0){
+            result.append(" and ");
+        }
+        if(hours==1){
+            result.append(hours + " hour");
+        }else if(hours>1) {
+            result.append(hours + " hours");
+        }
+        if(hours != 0 && minute != 0){
+            result.append(", ");
+        }
+        if(minute==1){
+            result.append(minute + " minute");
+        }else if(minute>1){
+            result.append(minute + " minutes");
+        }
+        if(minute != 0 && second != 0){
+            result.append(" and ");
+        }
+        if(second==1){
+            result.append(second + " second");
+        }else if(second>1){
+            result.append(second + " seconds");
+        }
+
         System.out.println(hours +" "+ minute + "" + second);
-        return "";
+        return result.toString();
     }
 }
