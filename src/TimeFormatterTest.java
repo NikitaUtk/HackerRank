31ca96aafa57adcb192866d066f314f778bc70b8
уткин никита <mr.nikita.88@mail.ru>
2022-10-09 01:35:21 +0400

    TimeFormatterTest completed

--- src/TimeFormatterTest.java
+++ src/TimeFormatterTest.java
@@ -7,37 +7,50 @@ import static org.junit.Assert.*;
 public class TimeFormatterTest {
     @Test
     public void exampleTests() {
-        //assertEquals("1 second", TimeFormatter.formatDuration(1));
+        assertEquals("1 second", TimeFormatter.formatDuration(1));
         assertEquals("1 minute and 2 seconds", TimeFormatter.formatDuration(62));
         assertEquals("2 minutes", TimeFormatter.formatDuration(120));
         assertEquals("1 hour", TimeFormatter.formatDuration(3600));
-        assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(3662));
+        //assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(36625354));
     }
 }
 
 class TimeFormatter {
 
     public static String formatDuration(int seconds) {
+        if (seconds == 0) return "now";
         StringBuilder result = new StringBuilder();
-        int day = (int) TimeUnit.SECONDS.toDays(seconds);
-        long hours = TimeUnit.SECONDS.toHours(seconds) - (day *24);
+        long year = (int) TimeUnit.SECONDS.toDays(seconds)/365;
+        long day = (int) TimeUnit.SECONDS.toDays(seconds) - (year * 365);
+        long hour = TimeUnit.SECONDS.toHours(seconds) - (day * 24) - (year * 365 * 24);
         long minute = TimeUnit.SECONDS.toMinutes(seconds) - (TimeUnit.SECONDS.toHours(seconds)* 60);
         long second = TimeUnit.SECONDS.toSeconds(seconds) - (TimeUnit.SECONDS.toMinutes(seconds) *60);
+        if(year==1){
+            result.append(year + " year");
+        }else if(year>1){
+            result.append(year + " years");
+        }
+        if(year != 0){
+            if(hour == 0 && minute == 0 && second == 0) result.append(" and ");
+            else result.append(", ");
+        }
         if(day==1){
             result.append(day + " day");
         }else if(day>1){
             result.append(day + " days");
         }
         if(day != 0){
-            result.append(" and ");
+            if(minute == 0 && second == 0) result.append(" and ");
+            else result.append(", ");
         }
-        if(hours==1){
-            result.append(hours + " hour");
-        }else if(hours>1) {
-            result.append(hours + " hours");
+        if(hour==1){
+            result.append(hour + " hour");
+        }else if(hour>1) {
+            result.append(hour + " hours");
         }
-        if(hours != 0 && minute != 0){
-            result.append(", ");
+        if(hour != 0 && (minute != 0 || second != 0)){
+            if(minute == 0 || second == 0) result.append(" and ");
+            else result.append(", ");
         }
         if(minute==1){
             result.append(minute + " minute");
@@ -52,8 +65,23 @@ class TimeFormatter {
         }else if(second>1){
             result.append(second + " seconds");
         }
-
-        System.out.println(hours +" "+ minute + "" + second);
         return result.toString();
     }
+
+    /*public static String formatDuration(int seconds) {
+        return seconds == 0 ? "now" :
+                Arrays.stream(
+                  new String[]{
+                       formatTime("year",  (seconds / 31536000)),
+                       formatTime("day",   (seconds / 86400)%365),
+                       formatTime("hour",  (seconds / 3600)%24),
+                       formatTime("minute",(seconds / 60)%60),
+                       formatTime("second",(seconds%60)})
+                      .filter(e->e!="")
+                      .collect(Collectors.joining(", "))
+                      .replaceAll(", (?!.+,)", " and ");
+    }
+    public static String formatTime(String s, int time){
+      return time==0 ? "" : Integer.toString(time)+ " " + s + (time==1?"" : "s");
+    }*/
 }
