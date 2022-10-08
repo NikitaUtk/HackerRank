3b4e63ff48c320fe1c467e4881ccf17339c1340b
уткин никита <nikita.utkn@gmail.com>
2022-10-09 01:45:06 +0400

    TimeFormatterTest completed

--- src/TimeFormatterTest.java
+++ src/TimeFormatterTest.java
@@ -11,7 +11,7 @@ public class TimeFormatterTest {
         assertEquals("1 minute and 2 seconds", TimeFormatter.formatDuration(62));
         assertEquals("2 minutes", TimeFormatter.formatDuration(120));
         assertEquals("1 hour", TimeFormatter.formatDuration(3600));
-        //assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(36625354));
+        assertEquals("1 hour, 1 minute and 2 seconds", TimeFormatter.formatDuration(36625354));
     }
 }
 
